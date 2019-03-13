e9c316c426e5686a332814e7f73f8da402ca0bfc
Tyler Gregg <hkrazy888@gmail.com>
2019-03-12 23:34:46 -0600

    feat: added uuid data types to elements

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const root = document.querySelector('#root')
 
 const vdom = {
     type:'div', attrs:{class:'hello'},props:{count:1, word:'bird'},children:[
-        {type:'h3', attrs:{},props:{},children:['hello world']},
+        {type:'h3', attrs:{},props:{},children:[{type:'text', text:'hello world'}]},
         {type:'div', attrs:{},props:{},children:[
-            {type:'p', attrs:{class:'filler', id: 'a'},props:{},children:['hi from the virtual dom, the count count is {{count}} {{count}}']},
-            {type:'p', attrs:{class:'filler', id: 'b'},props:{},children:['this is kinda fun eh? also the count is {{count}}']},
-            {type:'p', attrs:{class:'filler', id: 'a'},props:{},children:['the count is {{count}} and the word is {{word}}',
-                {type:'button',attrs:{id:'add',style:'margin:15px'},props:{},children:['Increase Count']}
+            {type:'p', attrs:{class:'filler', id: 'a'},props:{},children:[{type:'text', text:'hi from the virtual dom, the count count is {{count}} {{count}}'}]},
+            {type:'p', attrs:{class:'filler', id: 'b'},props:{},children:[{type:'text', text:'this is kinda fun eh? also the count is {{count}}'}]},
+            {type:'p', attrs:{class:'filler', id: 'a'},props:{},children:[{type:'text', text:'the count is {{count}} and the word is {{word}}'},
+                {type:'button',attrs:{id:'add',style:'margin:15px'},props:{},children:[{type:'text', text:'Increase Count'}]}
             ]}
         ]}
     ] 
--- src/frame.js
+++ src/frame.js
@@ -1,7 +1,16 @@
 export default class frame {
   constructor(params) {
-    this.vdom = params.vdom;
+    this.vdom = frame.setGuid(params.vdom);
+
     this.domCurrent = "";
+  }
+
+  static newGuid() {
+    return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, c => {
+      const r = (Math.random() * 16) | 0;
+      const v = c === "x" ? r : (r & 0x3) | 0x8;
+      return v.toString(16);
+    });
   }
 
   static replaceAll(str, find, replace) {
@@ -10,24 +19,29 @@ export default class frame {
 
   static setProps(v, propkey, propval) {
     v.children.forEach((child, i) => {
-      if (typeof child === "string") {
-        v.children[i] = frame.replaceAll(child, `{{${propkey}}}`, propval);
+      if (child.type === "text") {
+        v.children[i].text = frame.replaceAll(
+          child.text,
+          `{{${propkey}}}`,
+          propval
+        );
       } else {
         frame.setProps(child, propkey, propval);
       }
     });
   }
 
-  static render(v,selected){
-    let virtualdom = new frame({vdom:v});
-    virtualdom.domNew = virtualdom.renderVdom(v,selected);
-    virtualdom.domCurrent = virtualdom.domNew
-    console.log(virtualdom)
+  static render(v, selected) {
+    let virtualdom = new frame({ vdom: v });
+    virtualdom.domNew = virtualdom.renderVdom(v, selected);
+    virtualdom.domCurrent = virtualdom.domNew;
+    console.log(virtualdom);
   }
 
   renderVdom(v, selected) {
     // get element to append doc to, append vdom elt
     let elt = document.createElement(v.type);
+    elt.dataset.fId = v.fId;
     selected.appendChild(elt);
     // if there are attributes, set attributes
     if (v.attrs !== {}) {
@@ -40,18 +54,32 @@ export default class frame {
       // for each prop
       Object.keys(v.props).forEach(prop => {
         // for each child
-        frame.setProps(v,prop,v.props[prop])
+        frame.setProps(v, prop, v.props[prop]);
       });
     }
     // if text, render text, else, rerun renderVdom
     v.children.forEach(child => {
-      if (typeof child === "string") {
-        let text = document.createTextNode(child);
+      if (child.type === "text") {
+        let text = document.createTextNode(child.text);
         elt.appendChild(text);
       } else {
         this.renderVdom(child, elt);
       }
     });
-    return selected
+    return selected;
+  }
+
+  static setGuid(vdomelt) {
+    if (!vdomelt.fId) {
+      vdomelt.fId = frame.newGuid();
+    }
+    let gvdom = vdomelt.children.map(child => {
+      if (child.type !== "text" && !child.fId) {
+        child.fId = frame.newGuid();
+        this.setGuid(child);
+      }
+      return child;
+    });
+    return gvdom;
   }
 }
